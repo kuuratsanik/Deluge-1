f1b29eb2069bda4f6e7776951270602162e662d0
Calum Lind <calumlind+deluge@gmail.com>
2013-05-04 02:28:31 +0100

    update toolbar separators and build script

--- deluge/ui/web/js/deluge-all/Toolbar.js
+++ deluge/ui/web/js/deluge-all/Toolbar.js
@@ -43,9 +43,10 @@ Deluge.Toolbar = Ext.extend(Ext.Toolbar, {
                     id: 'tbar-deluge-text',
                     disabled: true,
                     text: _('Deluge'),
-                    iconCls: 'x-deluge-main-panel'
-                }, new Ext.Toolbar.Separator(),
-                {
+                    iconCls: 'x-deluge-main-panel',
+                },{
+                    xtype: 'tbseparator'
+                },{
                     id: 'create',
                     disabled: true,
                     hidden: true,
@@ -64,7 +65,9 @@ Deluge.Toolbar = Ext.extend(Ext.Toolbar, {
                     text: _('Remove'),
                     iconCls: 'icon-remove',
                     handler: this.onTorrentAction
-                }, new Ext.Toolbar.Separator(),{
+                },{
+                    xtype: 'tbseparator'
+                },{
                     id: 'pause',
                     disabled: true,
                     text: _('Pause'),
@@ -76,7 +79,9 @@ Deluge.Toolbar = Ext.extend(Ext.Toolbar, {
                     text: _('Resume'),
                     iconCls: 'icon-resume',
                     handler: this.onTorrentAction
-                }, new Ext.Toolbar.Separator(),{
+                },{
+                    xtype: 'tbseparator'
+                },{
                     id: 'up',
                     cls: 'x-btn-text-icon',
                     disabled: true,
@@ -89,7 +94,9 @@ Deluge.Toolbar = Ext.extend(Ext.Toolbar, {
                     text: _('Down'),
                     iconCls: 'icon-down',
                     handler: this.onTorrentAction
-                }, new Ext.Toolbar.Separator(),{
+                },{
+                    xtype: 'tbseparator'
+                },{
                     id: 'preferences',
                     text: _('Preferences'),
                     iconCls: 'x-deluge-preferences',
