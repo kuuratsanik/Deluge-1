deluge-torrent/deluge

details: add the missing items to the details tab
The web interface was missing the Owner and Shared items
that have been added to the gtk ui. Add these to bring the web
interface up to scratch.

deluge/ui/web/js/deluge-all/Keys.js
@@ -88,7 +88,7 @@ Deluge.Keys = {
      */
     Details: [
         'name', 'save_path', 'total_size', 'num_files', 'message',
-        'tracker', 'comment'
+        'tracker', 'comment', 'owner', 'shared'
     ],
 
     /**

kuuratsanik/Deluge-1

deluge/ui/web/js/deluge-all/Keys.js
@@ -91,7 +91,7 @@ Deluge.Keys = {
      */
     Details: [
         'name', 'download_location', 'total_size', 'num_files', 'message',
-        'tracker_host', 'comment'
+        'tracker_host', 'comment', 'owner', 'shared'
     ],
 
     /**